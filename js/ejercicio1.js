c719d7218ab946afad17c8797e283e65df55ef52
Daniela Brown <daniimbrown@gmail.com>
2025-01-16 15:04:14 -0300

    Creada estructura.

---
+++ js/ejercicio1.js
@@ -0,0 +1,1 @@
+// 1. Dado un array de números [10, 20, 30, 40, 50, 60], crea un programa que calcule e imprima el promedio de los números en el array. 
---
+++ js/ejercicio2.js
@@ -0,0 +1,1 @@
+// 2. Dado un array de palabras, crea un programa que devuelva un nuevo array con palabras que tengan más de 5 letras. Por ejemplo: ["casa", "programación", "sol", "javascript", “texto”, “html”, “bootstrap”, “css”, “nodejs”] debería devolver ["programación", "javascript"]. 
